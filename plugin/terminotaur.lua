ea327bb0ab4a8c29324f197f54ab64cf1543ab5a
Athul Anoop <athulanoop2020@gmail.com>
2025-02-03 20:19:07 +0530

    update: removed debug statement

--- plugin/terminotaur.lua
+++ plugin/terminotaur.lua
@@ -1,6 +1,4 @@
 -- luacheck: globals vim
-
-print("terminotaur.nvim loaded")
 vim.api.nvim_create_user_command("ToggleFloatingTerminal", require("terminotaur.floating_terminal").toggle, {})
 vim.api.nvim_create_user_command("OpenNewVerticalTerminal", require("terminotaur.split_terminal").vertical, {})
 vim.api.nvim_create_user_command("OpenNewHorizontalTerminal", require("terminotaur.split_terminal").horizontal, {})
